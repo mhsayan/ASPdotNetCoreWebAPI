7848f737d9edc89f81f5f806439c4624a61cc2e5
Md. Mahmudul Hasan <mhsayan.ltd@gmail.com>
2022-02-08 01:48:00 +0600

    updated appsettings.json for logger

--- Application/Application.Api/Program.cs
+++ Application/Application.Api/Program.cs
@@ -65,7 +65,6 @@ try
     }
     else
     {
-        app.UseExceptionHandler("/Home/Error");
         // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
         app.UseHsts();
     }
