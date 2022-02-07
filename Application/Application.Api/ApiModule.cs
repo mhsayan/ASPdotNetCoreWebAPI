0d63b1535379bb6f5c8e0272515504f1a63c2f97
Md. Mahmudul Hasan <mhsayan.ltd@gmail.com>
2022-02-08 02:04:14 +0600

    Added profile and module class in API project

---
+++ Application/Application.Api/ApiModule.cs
@@ -0,0 +1,13 @@
+﻿using Autofac;
+
+namespace Application.Api;
+
+public class ApiModule : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        // builder.RegisterType<IndexModel>().AsSelf();
+
+        base.Load(builder);
+    }
+}
---
+++ Application/Application.Api/Profiles/ApiProfile.cs
@@ -0,0 +1,11 @@
+﻿using AutoMapper;
+
+namespace Application.Api.Profiles;
+
+public class ApiProfile : Profile
+{
+    public ApiProfile()
+    {
+        // CreateMap<CreateQuestionModel, BO.Question>().ReverseMap();
+    }
+}
